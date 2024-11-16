715bcaada5ddf8d3de7b52cb76e866d8bfb8cd91
Abbas Raza <abbasrazaa02@gmail.com>
2024-11-16 23:39:22 +0500

    Added Authorization Controller

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,74 +1,76 @@
 // import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import AuthLayout from './components/auth/layout'
-import AuthLogin from './pages/auth/login'
-import AuthRegister from './pages/auth/register'
-import AdminLayout from './components/admin-view/layout'
-import AdminDashboard from './pages/admin-view/dashboard'
-import AdminProducts from './pages/admin-view/products'
-import AdminFeatures from './pages/admin-view/features'
-import AdminOrders from './pages/admin-view/orders'
-import ShoppingLayout from './components/shopping-view/layout'
-import NotFound from './pages/not-found'
-import ShoppingHome from './pages/shopping-view/home'
-import ShoppingListing from './pages/shopping-view/listing'
-import ShoppingAccount from './pages/shopping-view/account'
-import ShoppingCheckout from './pages/shopping-view/checkout'
-import CheckAuth from './components/common/check-auth'
-
+import { Routes, Route } from "react-router-dom";
+import AuthLayout from "./components/auth/layout";
+import AuthLogin from "./pages/auth/login";
+import AuthRegister from "./pages/auth/register";
+import AdminLayout from "./components/admin-view/layout";
+import AdminDashboard from "./pages/admin-view/dashboard";
+import AdminProducts from "./pages/admin-view/products";
+import AdminFeatures from "./pages/admin-view/features";
+import AdminOrders from "./pages/admin-view/orders";
+import ShoppingLayout from "./components/shopping-view/layout";
+import NotFound from "./pages/not-found";
+import ShoppingHome from "./pages/shopping-view/home";
+import ShoppingListing from "./pages/shopping-view/listing";
+import ShoppingAccount from "./pages/shopping-view/account";
+import ShoppingCheckout from "./pages/shopping-view/checkout";
+import CheckAuth from "./components/common/check-auth";
 
 function App() {
-
   const isAuthenticated = true;
   const user = {
-    name: 'Abbas Raza',
-    role: 'user'
-  }
+    name: "Abbas Raza",
+    role: "user",
+  };
 
   return (
     <div className="flex flex-col overflow-hidden bg-white">
       <h1>Header</h1>
       <Routes>
-        <Route path="/auth" element={
-          <CheckAuth isAuthenticated={isAuthenticated} user={user} >
-            <AuthLayout />
-          </CheckAuth>
-        } >
-
-          <Route path="login" element={<AuthLogin/>} />
-          <Route path="register" element={<AuthRegister/>} />
+        <Route
+          path="/auth"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AuthLayout />
+            </CheckAuth>
+          }
+        >
+          <Route path="login" element={<AuthLogin />} />
+          <Route path="register" element={<AuthRegister />} />
         </Route>
-        <Route path='/admin' element={
-          <CheckAuth isAuthenticated={isAuthenticated} user={user} >
-            <AdminLayout />
-          </CheckAuth>
-        } >
-          <Route path='dashboard' element={<AdminDashboard/>} />
-          <Route path='products' element={<AdminProducts/>} />
-          <Route path='features' element={<AdminFeatures/>} />
-          <Route path='orders' element={<AdminOrders/>} />
-          <Route path='/shop' element={
-            <CheckAuth isAuthenticated={isAuthenticated} user={user} >
+
+        <Route
+          path="/admin"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AdminLayout />
+            </CheckAuth>
+          }
+        >
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="features" element={<AdminFeatures />} />
+          <Route path="orders" element={<AdminOrders />} />
+        </Route>
+
+        <Route
+          path="/shop"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
               <ShoppingLayout />
             </CheckAuth>
-          }>
-            <Route path='home' element={<ShoppingHome/>}/>
-
-            <Route path='listing' element={<ShoppingListing/>} />
-
-
-            <Route path='checkout' element={<ShoppingCheckout/>} />
-
-            <Route path='account' element={<ShoppingAccount/>} />
-          
-          </Route>
-          <Route path='*' element={<NotFound/>}/>
-
+          }
+        >
+          <Route path="home" element={<ShoppingHome />} />
+          <Route path="listing" element={<ShoppingListing />} />
+          <Route path="checkout" element={<ShoppingCheckout />} />
+          <Route path="account" element={<ShoppingAccount />} />
         </Route>
-      </Routes>
 
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
