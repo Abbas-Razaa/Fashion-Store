284b3c3b985b1879c9a8c2342fbfa308e26bc02e
Abbas Raza <abbasrazaa02@gmail.com>
2024-11-17 22:45:47 +0500

    created auth route

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -7,12 +7,12 @@ const registerUser = async (req, res) => {
   const { userName, email, password } = req.body;
 
   try {
-    // const checkUser = await User.findOne({ email });
-    // if (checkUser)
-    //   return res.json({
-    //     success: false,
-    //     message: "User Already exists with the same email! Please try again",
-    //   });
+    const checkUser = await User.findOne({ email });
+    if (checkUser)
+      return res.json({
+        success: false,
+        message: "User Already exists with the same email! Please try again",
+      });
 
     const hashPassword = await bcrypt.hash(password, 12);
     const newUser = new User({
@@ -40,44 +40,44 @@ const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // const checkUser = await User.findOne({ email });
-    // if (!checkUser)
-    //   return res.json({
-    //     success: false,
-    //     message: "User doesn't exists! Please register first",
-    //   });
+    const checkUser = await User.findOne({ email });
+    if (!checkUser)
+      return res.json({
+        success: false,
+        message: "User doesn't exists! Please register first",
+      });
 
-    // const checkPasswordMatch = await bcrypt.compare(
-    //   password,
-    //   checkUser.password
-    // );
-    // if (!checkPasswordMatch)
-    //   return res.json({
-    //     success: false,
-    //     message: "Incorrect password! Please try again",
-    //   });
+    const checkPasswordMatch = await bcrypt.compare(
+      password,
+      checkUser.password
+    );
+    if (!checkPasswordMatch)
+      return res.json({
+        success: false,
+        message: "Incorrect password! Please try again",
+      });
 
-    // const token = jwt.sign(
-    //   {
-    //     id: checkUser._id,
-    //     role: checkUser.role,
-    //     email: checkUser.email,
-    //     userName: checkUser.userName,
-    //   },
-    //   "CLIENT_SECRET_KEY",
-    //   { expiresIn: "60m" }
-    // );
+    const token = jwt.sign(
+      {
+        id: checkUser._id,
+        role: checkUser.role,
+        email: checkUser.email,
+        userName: checkUser.userName,
+      },
+      "CLIENT_SECRET_KEY",
+      { expiresIn: "60m" }
+    );
 
-    // res.cookie("token", token, { httpOnly: true, secure: false }).json({
-    //   success: true,
-    //   message: "Logged in successfully",
-    //   user: {
-    //     email: checkUser.email,
-    //     role: checkUser.role,
-    //     id: checkUser._id,
-    //     userName: checkUser.userName,
-    //   },
-    // });
+    res.cookie("token", token, { httpOnly: true, secure: false }).json({
+      success: true,
+      message: "Logged in successfully",
+      user: {
+        email: checkUser.email,
+        role: checkUser.role,
+        id: checkUser._id,
+        userName: checkUser.userName,
+      },
+    });
   } catch (e) {
     console.log(e);
     res.status(500).json({
@@ -106,9 +106,9 @@ const authMiddleware = async (req, res, next) => {
     });
 
   try {
-    // const decoded = jwt.verify(token, "CLIENT_SECRET_KEY");
-    // req.user = decoded;
-    // next();
+    const decoded = jwt.verify(token, "CLIENT_SECRET_KEY");
+    req.user = decoded;
+    next();
   } catch (error) {
     res.status(401).json({
       success: false,
