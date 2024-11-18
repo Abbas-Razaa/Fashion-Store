33c44f93b4437ccf934875fe32eda10665e14a2b
Abbas Raza <abbasrazaa02@gmail.com>
2024-11-18 23:44:50 +0500

    made adjustment on server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,35 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const authRouter = require("./routes/auth/auth-routes");
+const adminProductsRouter = require("./routes/admin/products-routes");
+const adminOrderRouter = require("./routes/admin/order-routes");
 
+const shopProductsRouter = require("./routes/shop/products-routes");
+const shopCartRouter = require("./routes/shop/cart-routes");
+const shopAddressRouter = require("./routes/shop/address-routes");
+const shopOrderRouter = require("./routes/shop/order-routes");
+const shopSearchRouter = require("./routes/shop/search-routes");
+const shopReviewRouter = require("./routes/shop/review-routes");
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://abbasrazaa02:30FWR6LU2rMBYJuw@cluster0.brzd5.mongodb.net/').then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+const commonFeatureRouter = require("./routes/common/feature-routes");
 
+//create a database connection -> u can also
+//create a separate file for this and then import/use that file here
+
+mongoose
+  .connect("db_url")
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // allow to server to accept request from different origin
-    methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"],
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
       "Authorization",
@@ -23,13 +37,23 @@ app.use(
       "Expires",
       "Pragma",
     ],
-    credentials: true, // allow session cookie from browser to pass through
+    credentials: true,
   })
 );
 
 app.use(cookieParser());
 app.use(express.json());
+app.use("/api/auth", authRouter);
+app.use("/api/admin/products", adminProductsRouter);
+app.use("/api/admin/orders", adminOrderRouter);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use("/api/shop/products", shopProductsRouter);
+app.use("/api/shop/cart", shopCartRouter);
+app.use("/api/shop/address", shopAddressRouter);
+app.use("/api/shop/order", shopOrderRouter);
+app.use("/api/shop/search", shopSearchRouter);
+app.use("/api/shop/review", shopReviewRouter);
 
+app.use("/api/common/feature", commonFeatureRouter);
 
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
