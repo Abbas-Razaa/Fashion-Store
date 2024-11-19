4bd664a7ad1e36042cb9c35be4754874a90cef76
Abbas Raza <abbasrazaa02@gmail.com>
2024-11-19 22:34:06 +0500

    Registration Completed

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,7 +1,7 @@
 import CommonForm from "@/components/common/form";
-// import { useToast } from "@/components/ui/use-toast";
+import { useToast } from "@/components/ui/use-toast";
 import { registerFormControls } from "@/config";
-// import { registerUser } from "@/store/auth-slice";
+import { registerUser } from "@/store/auth-slice";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
@@ -19,20 +19,20 @@ function AuthRegister() {
   const { toast } = useToast();
 
   function onSubmit(event) {
-    // event.preventDefault();
-    // dispatch(registerUser(formData)).then((data) => {
-    //   if (data?.payload?.success) {
-    //     toast({
-    //       title: data?.payload?.message,
-    //     });
-    //     navigate("/auth/login");
-    //   } else {
-    //     toast({
-    //       title: data?.payload?.message,
-    //       variant: "destructive",
-    //     });
-    //   }
-    // });
+    event.preventDefault();
+    dispatch(registerUser(formData)).then((data) => {
+      if (data?.payload?.success) {
+        toast({
+          title: data?.payload?.message,
+        });
+        navigate("/auth/login");
+      } else {
+        toast({
+          title: data?.payload?.message,
+          variant: "destructive",
+        });
+      }
+    });
   }
 
   console.log(formData);
