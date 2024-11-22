de6c1b0ec04a7d01f30f27eacb8e4ea7a42d771b
Abbas Raza <abbasrazaa02@gmail.com>
2024-11-22 20:35:40 +0500

    Created Admin View

--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -1,15 +1,19 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
-import AdminSidebar from "./sidebar";
+import AdminSideBar from "./sidebar";
 import AdminHeader from "./header";
+import { useState } from "react";
 
 function AdminLayout() {
+  const [openSidebar, setOpenSidebar] = useState(false);
+
   return (
     <div className="flex min-h-screen w-full">
-      <AdminSidebar />
+      {/* admin sidebar */}
+      <AdminSideBar open={openSidebar} setOpen={setOpenSidebar} />
       <div className="flex flex-1 flex-col">
-        <AdminHeader />
-        <main className="flex-1 flex bg-muted/40 p-4 md:p-6">
+        {/* admin header */}
+        <AdminHeader setOpen={setOpenSidebar} />
+        <main className="flex-1 flex-col flex bg-muted/40 p-4 md:p-6">
           <Outlet />
         </main>
       </div>
