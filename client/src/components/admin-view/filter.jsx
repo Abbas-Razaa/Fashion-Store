fcd9ed526f4ce4bc59945ebea9892dbfc1213f6e
Abbas Raza <abbasrazaa02@gmail.com>
2024-12-09 01:18:27 +0500

    created filter.jsx

---
+++ client/src/components/admin-view/filter.jsx
@@ -0,0 +1,13 @@
+function ProductFilter() {
+  return (
+    <div className="bg-background rounded-lg shadow-sm">
+      <div className="p-4 border-b">
+        <h2 className="text-lg font-extrabold">Filters</h2>
+      </div>
+      <div className="p-4 space-y-4">
+      </div>
+    </div>
+  );
+}
+
+export default ProductFilter;
