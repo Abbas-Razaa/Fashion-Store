02ee6c1f16674a78d3fc74a8ce0e38e6a2e8bc70
Abbas Raza <abbasrazaa02@gmail.com>
2024-12-10 23:39:57 +0500

    wrote filter options

--- client/src/components/admin-view/filter.jsx
+++ client/src/components/admin-view/filter.jsx
@@ -1,3 +1,8 @@
+import { filterOptions } from "@/config";
+import { Fragment } from "react";
+import { Label } from "../ui/label";
+import { Separator } from "../ui/separator";
+
 function ProductFilter() {
   return (
     <div className="bg-background rounded-lg shadow-sm">
@@ -5,6 +10,20 @@ function ProductFilter() {
         <h2 className="text-lg font-extrabold">Filters</h2>
       </div>
       <div className="p-4 space-y-4">
+        {Object.keys(filterOptions).map((keyItem) => (
+          <Fragment>
+            <div>
+              <h3 className="text-base font-bold">{keyItem}</h3>
+              <div className="grid gap-2 mt-2">
+                {filterOptions[keyItem].map((option) => (
+                  <Label className="flex font-medium items-center gap-2 ">
+                  </Label>
+                ))}
+              </div>
+            </div>
+            <Separator />
+          </Fragment>
+        ))}
       </div>
     </div>
   );
