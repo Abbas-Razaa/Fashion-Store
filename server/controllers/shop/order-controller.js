493f1bcab9979124baf30dbd17d49f293b25f2e3
Abbas Raza <abbasrazaa02@gmail.com>
2025-01-05 23:13:56 +0500

    removed paypal

--- server/controllers/shop/order-controller.js
+++ server/controllers/shop/order-controller.js
@@ -1,4 +1,3 @@
-// const paypal = require("../../helpers/paypal");
 const Order = require("../../models/Order");
 const Cart = require("../../models/Cart");
 const Product = require("../../models/Product");
@@ -6,18 +5,8 @@ const Product = require("../../models/Product");
 const createOrder = async (req, res) => {
   try {
     const {
-      userId,
       cartItems,
-      addressInfo,
-      orderStatus,
-      paymentMethod,
-      paymentStatus,
       totalAmount,
-      orderDate,
-      orderUpdateDate,
-      paymentId,
-      payerId,
-      cartId,
     } = req.body;
 
     const create_payment_json = {
@@ -48,44 +37,6 @@ const createOrder = async (req, res) => {
         },
       ],
     };
-
-    // paypal.payment.create(create_payment_json, async (error, paymentInfo) => {
-    //   if (error) {
-    //     console.log(error);
-
-    //     return res.status(500).json({
-    //       success: false,
-    //       message: "Error while creating paypal payment",
-    //     });
-    //   } else {
-    //     const newlyCreatedOrder = new Order({
-    //       userId,
-    //       cartId,
-    //       cartItems,
-    //       addressInfo,
-    //       orderStatus,
-    //       paymentMethod,
-    //       paymentStatus,
-    //       totalAmount,
-    //       orderDate,
-    //       orderUpdateDate,
-    //       paymentId,
-    //       payerId,
-    //     });
-
-    //     await newlyCreatedOrder.save();
-
-    //     const approvalURL = paymentInfo.links.find(
-    //       (link) => link.rel === "approval_url"
-    //     ).href;
-
-    //     res.status(201).json({
-    //       success: true,
-    //       approvalURL,
-    //       orderId: newlyCreatedOrder._id,
-    //     });
-    //   }
-    // });
   } catch (e) {
     console.log(e);
     res.status(500).json({
